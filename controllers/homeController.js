1dbad003c940278625f1eb15fb313e59f1c38e31
Rosen Yorgov <iorgovaa@gmail.com>
2022-01-06 14:53:23 +0200

    added thank you page and removed recaptcha after sending email

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -15,8 +15,13 @@ const contactme = (req, res) => {
    res.render('contactme/contactme');
 };
 
+const thankyou = (req, res) => {
+   res.render('thankyou/thankyou');
+};
+
 router.get('/', home);
 router.get('/about', about);
 router.get('/contactme', contactme);
+router.get('/thankyou', thankyou);
 
 module.exports = router;
